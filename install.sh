519b07705a6aa8075bf09a398e5120a4de2190fd
TsCl <pmontagnier@gmail.com>
2014-04-28 22:56:44 +0200

    Little mistake

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #/bin/bash
 echo 'Linking oh-my-zsh files'
-ln -s $(PWD)/oh-my-zsh/pmontagnier.zshenv.zsh ~/.oh-my-zsh/custom/pmontagnier.zshenv.zsh
-ln -s $(PWD)/oh-my-zsh/pmontagnier.zshrc.zsh ~/.oh-my-zsh/custom/pmontagnier.zshrc.zsh
-ln -s $(PWD)/oh-my-zsh/pmontagnier.zsh-theme ~/.oh-my-zsh/themes/pmontagnier.zsh-theme
+ln -s $PWD/oh-my-zsh/pmontagnier.zshenv.zsh ~/.oh-my-zsh/custom/pmontagnier.zshenv.zsh
+ln -s $PWD/oh-my-zsh/pmontagnier.zshrc.zsh ~/.oh-my-zsh/custom/pmontagnier.zshrc.zsh
+ln -s $PWD/oh-my-zsh/pmontagnier.zsh-theme ~/.oh-my-zsh/themes/pmontagnier.zsh-theme
 
 echo 'Linking dotfiles'
-ln -s $(PWD)/oh-my-zsh/.zshrc ~/.zshrc
-ln -s $(PWD)/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s $(PWD)/dotfiles/.bash_profile ~/.bash_profile
-ln -s $(PWD)/dotfiles/.bashrc ~/.bashrc
-ln -s $(PWD)/dotfiles/.hgrc ~/.hgrc
-ln -s $(PWD)/dotfiles/.screenrc ~/.screenrc
-ln -s $(PWD)/dotfiles/.vimrc ~/.vimrc
+ln -s $PWD/oh-my-zsh/.zshrc ~/.zshrc
+ln -s $PWD/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s $PWD/dotfiles/.bash_profile ~/.bash_profile
+ln -s $PWD/dotfiles/.bashrc ~/.bashrc
+ln -s $PWD/dotfiles/.hgrc ~/.hgrc
+ln -s $PWD/dotfiles/.screenrc ~/.screenrc
+ln -s $PWD/dotfiles/.vimrc ~/.vimrc
 
 echo "Done"
 exit 0
